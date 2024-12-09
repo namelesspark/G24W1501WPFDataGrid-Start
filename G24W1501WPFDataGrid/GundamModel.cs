8e1a644d6eb85ceda82cf6535f2432b24261155e
박민호 <20200432@office.kumoh.ac.kr>
2024-12-09 14:32:53 +0900

    초기 커밋.

---
+++ G24W1501WPFDataGrid/GundamModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace G24W1501WPFDataGrid;
+
+public class GundamModel
+{
+    public GundamModel(string name, string model, string party) =>  //s는 비주얼 스튜디오의 기본 키워드 / S는 .NET의 클래스
+        (Name, Model, Party) = (name, model, party);
+
+    public string Name { get; }
+    public string Model { get; }
+    public string Party { get; }
+
+
+}
---
+++ G24W1501WPFDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace G24W1501WPFDataGrid;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{   
+    protected List<GundamModel> GundamList = new List<GundamModel>();
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        GundamGrid.ItemsSource = GundamList;
+
+        GundamList.Add(new GundamModel("건담", "RX-78-2", "연방군"));
+        GundamList.Add(new GundamModel("자쿠II", "MS-06", "지온군"));
+    }
+
+    public void OnAdd(object sender, RoutedEventArgs e)
+    {
+        GundamList.Add(new GundamModel("건탱크", "RX-75-4", "연방군"));
+        GundamGrid.Items.Refresh();
+    }
+}
